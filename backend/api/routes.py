7f4097ea48b0165fa8c04b472c7062c68915a83c
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-10-23 23:25:08 +0530

    Update routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -5,7 +5,6 @@ from api.controllers.refunds_controller import app as refunds_app
 
 app = Flask(__name__)
 
-# Register the blueprints
 app.register_blueprint(projects_app)
 app.register_blueprint(milestones_app)
 app.register_blueprint(refunds_app)
