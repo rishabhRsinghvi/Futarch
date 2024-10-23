3828072372056e3111c80428fc53c38934f80f61
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-10-23 14:04:30 +0530

    Update routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from controllers.projects_controller import app as projects_app
+from controllers.milestones_controller import app as milestones_app
+from controllers.refunds_controller import app as refunds_app
 
+app = Flask(__name__)
+app.register_blueprint(projects_app)
+app.register_blueprint(milestones_app)
+app.register_blueprint(refunds_app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
